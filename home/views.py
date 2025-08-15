835c9312e11eb4147ee753136db69778d17b57fe
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-08-15 20:32:55 +0100

    AboutMe and Version

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+
 
 # Create your views here.
+def credits(request):
+    content = "Nicky\nRuairi"
+    return HttpResponse(content, content_type="text/plain")
+
+
+def about(request):
+    content = "<h1>About Me!</h1>"
+    return HttpResponse(content, content_type="text/html")
+
+
+def version_info(request):
+    content = {"version": "1.0"}
+    content = json.dumps(content)
+    return HttpResponse(content, content_type="json")
